0f4895af4cb6e5be8658de2cb4322c58aba5c2b9
Stoshi Murakami <ssmr9dt@gmail.com>
2018-11-22 06:01:02 +0000

    forever command added

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ cd mtgroom-opencv/
 /usr/bin/git reset --hard
 
 /root/.nvm/versions/node/v10.10.0/bin/npm install
-/root/.nvm/versions/node/v10.10.0/bin/npm start &
+/root/.nvm/versions/node/v10.10.0/bin/forever start run.js
