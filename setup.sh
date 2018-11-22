776f65e52c360479f396fe5fd0638c0c3eacc122
Stoshi Murakami <ssmr9dt@gmail.com>
2018-11-22 01:24:09 +0000

    setup.sh added

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# alias npm='/root/.nvm/versions/node/v10.10.0/bin/npm'
+# alias node='/root/.nvm/versions/node/v10.10.0/bin/node'
+
+cd mtgroom-opencv/
+
+/usr/bin/git pull origin master
+/usr/bin/git reset --hard
+
+/root/.nvm/versions/node/v10.10.0/bin/npm install
+/root/.nvm/versions/node/v10.10.0/bin/npm start &
