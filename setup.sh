c0dbaa22cf2648e697d35986ecb5fa497efa5fe3
Stoshi Murakami <ssmr9dt@gmail.com>
2018-11-26 13:35:24 +0000

    setup.sh update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# alias npm='/root/.nvm/versions/node/v10.10.0/bin/npm'
-# alias node='/root/.nvm/versions/node/v10.10.0/bin/node'
+export PATH=/root/.nvm/versions/node/v10.10.0/bin:$PATH
 
 cd mtgroom-opencv/
 
 /usr/bin/git pull origin master
 /usr/bin/git reset --hard
 
-/root/.nvm/versions/node/v10.10.0/bin/npm install > setup.log 2>&1
-/root/.nvm/versions/node/v10.10.0/bin/forever start /root/mtgroom-opencv/run.js
+npm install > setup.log 2>&1
+forever start run.js
