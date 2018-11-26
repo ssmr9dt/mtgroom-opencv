982f667ce4da11b927cda2d0d24f7ad0e78d3240
Stoshi Murakami <ssmr9dt@gmail.com>
2018-11-26 13:29:59 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ cd mtgroom-opencv/
 /usr/bin/git pull origin master
 /usr/bin/git reset --hard
 
-/root/.nvm/versions/node/v10.10.0/bin/npm install
+/root/.nvm/versions/node/v10.10.0/bin/npm install > setup.log 2>&1
 /root/.nvm/versions/node/v10.10.0/bin/forever start /root/mtgroom-opencv/run.js
