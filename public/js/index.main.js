608a7aeb9873d7418f7b4e7728483e9917b85c19
Stoshi Murakami <ssmr9dt@gmail.com>
2018-11-28 06:32:45 +0000

    main.js update in client side

--- public/js/index.main.js
+++ public/js/index.main.js
@@ -2,10 +2,19 @@
 
 var socket = io();
 
-socket.on("photo", function(face){
-  document.getElementById("photo").src = "data:image/jpg;base64," +face.buffer;
+socket.on("photo", function(face) {
+  // document.getElementById("photo").src = "data:image/jpg;base64," +face.buffer;
 });
 
 socket.on("status", function(e) {
-  document.getElementById("num").innerHTML = e.enter === true ? e.num + "人いる" : "誰もいない";
+  const empty_style = document.getElementById("empty").style;
+  const occupied_style = document.getElementById("occupied").style;
+  
+  if (!!e.enter) {
+    empty_style.display = "none";
+    occupied_style.display = "block";
+  } else {
+    empty_style.display = "block";
+    occupied_style.display = "none";
+  }
 });
